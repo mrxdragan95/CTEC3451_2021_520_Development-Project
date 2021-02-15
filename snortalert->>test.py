ae99960599470543ae442d922c6d25527a7d4b4b
mrxdragan95 <57604560+mrxdragan95@users.noreply.github.com>
2021-02-15 14:56:19 +0000

    Update snortalert->>test.py

--- snortalert->>test.py
+++ snortalert->>test.py
@@ -1,4 +1,20 @@
-import os
+mport os, glob
 
-os.system('grep -n 10000 /var/log/snort/alert > /tmp/test6.txt')
-os.system('cat /var/log/snort/alert > /tmp/testfull.txt')
+file_path = "/tmp/test/"
+test = os.path.dirname(file_path)
+
+try:
+    os.stat(test)
+except:
+    os.mkdir(test)
+
+#Loop Through the folder projects all files and deleting them one by one
+for file in glob.glob("/tmp/test/*"):
+    os.remove(file)
+    print("Deleted " + str(file))
+else:
+  print("The file does not exist")
+
+os.system('grep -n 10000 /var/log/snort/alert > /tmp/test/test.txt')
+os.system('cat /var/log/snort/alert > /tmp/test/testfull.txt')
+print("Done!")
