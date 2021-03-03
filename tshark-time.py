504a17488ab9e42ba5ac27fc3e97639a39927af9
mrxdragan95 <57604560+mrxdragan95@users.noreply.github.com>
2021-03-03 18:38:19 +0000

    Update tshark-time.py

--- tshark-time.py
+++ tshark-time.py
@@ -7,12 +7,26 @@ import os.path
 import shutil
 from datetime import datetime
 
+def new_folder_log(curr_time):
+    func_name = "New folder to /tmp/snortlog"
+    print(func_name)
+    file_path = "/tmp/snortlog" + curr_time + "/"
+    snortfile = os.path.dirname(file_path)
+
+    try:
+        os.stat(snortfile)
+    except:
+        os.mkdir(snortfile)
+
+    print("New folder is done")
+
+
 def run_tshark_on_local_machine(curr_time):
     func_name = "run_tshark_on_local_machine - "
     print(func_name + "start")
     interface_name = "eth0"
-    capture_file_name = "/tmp/Capture_interface_" + interface_name + "_" + curr_time + ".pcap"
-    num_sec_to_sleep = 10
+    capture_file_name = "/tmp/snortlog" + curr_time + "/" "Capture_" + interface_name + "_" + curr_time + ".pcap"
+    num_sec_to_sleep = 90
     print(func_name + "about to create capture with name:" + capture_file_name)
     p = subprocess.Popen(["tshark",
                           "-i", interface_name,
@@ -29,7 +43,7 @@ def run_snort(curr_time):
     interface_name = "eth0"
     snortlog = "/var/log/snort"
     snortconf = "/etc/snort/snort.conf"
-    capture_file_name = "/tmp/Capture_interface_" + interface_name+ "_" + curr_time + ".pcap"
+    capture_file_name = "/tmp/snortlog" + curr_time + "/" "Capture_" + interface_name + "_" + curr_time + ".pcap"
     num_sec_to_sleep = 20
     s = subprocess.Popen(["snort", 
                           "-l", snortlog, "-c",
@@ -41,27 +55,12 @@ def run_snort(curr_time):
     s.terminate()
 
 def snortlog_to_txt(curr_time):
-    func_name = "Move to /tmp/snortlog"
-    print(func_name)
-    file_path = "/tmp/snortlog" + curr_time + "/"
-    snortfile = os.path.dirname(file_path)
-
-    try:
-        os.stat(snortfile)
-    except:
-        os.mkdir(snortfile)
 
     file_line = "/tmp/snortlog" + curr_time + "/snortlog_line.txt"
     file = open(file_line, "w")
     file.close()
     os.chmod(file_line, stat.S_IRWXU|stat.S_IRWXG|stat.S_IRWXO)   
     shutil.copy('/var/log/snort/alert', file_line)
-
-    #file_cat = "/tmp/snortlog" + curr_time + "/snortlog_cat.txt"
-    #file = open(file_cat, "w")
-    #file.close()
-    #os.chmod(file_cat, stat.S_IRWXU|stat.S_IRWXG|stat.S_IRWXO)
-    #shutil.copy('/var/log/snort/alert', cat_line)
 
 def barnyard2_to_csv(curr_time):
     func_name = "run_barnyard2 - "
@@ -89,6 +88,7 @@ def barnyard2_to_csv(curr_time):
 
 curr_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 os.system('systemctl stop snort')
+new_folder_log(curr_time)
 run_tshark_on_local_machine(curr_time)
 run_snort(curr_time)
 snortlog_to_txt(curr_time)
