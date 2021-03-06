5842b71f74359317e79e5b18e4920e1b3c788f25
mrxdragan95 <57604560+mrxdragan95@users.noreply.github.com>
2021-03-06 02:39:36 +0000

    Update tshark.py

--- tshark.py
+++ tshark.py
@@ -67,64 +67,67 @@ def run_tshark_on_local_machine(curr_time):
 def run_snort(curr_time):
     func_name = "Snort - "
     print(func_name + "start")
-    #
+    #Reading the alert log directory
     snortlog = "/var/log/snort"
-    #
+    #Reading the variety of Snort configuration options that can be set 
     snortconf = "/etc/snort/snort.conf"
-    #
+    #Reading a new packet capture from tshark with curr_time
     capture_file_name = "/tmp/snortlog" + curr_time + "/" "Capture_" + interface_name + "_" + curr_time + ".pcap"
-    #
+    #Waiting for 115 seconds while snort running
     num_sec_to_sleep = 15
-    #
+    #Executing the snort with packet capture and putting into the snort.log and alert
     snort = subprocess.Popen(["snort", 
                           "-l", snortlog, "-c",
 			  snortconf, "-r",
 			  capture_file_name,
 			  "-A", "fast"],
                           stdout=subprocess.PIPE)
-    #
+    #Stopping snort in time.sleep
     time.sleep(num_sec_to_sleep)
-    #
+    #Exiting the program successfully Or Stopping a running python and creating log
     snort.terminate()
+    print(func_name + "end")
 
+# snortlog to text Function---------------------------------------------------------------------------------------------------------
 def snortlog_to_txt(curr_time):
-    #
+    #Creating new snortlog.txt and open file in current directory with current date
     file_line = "/tmp/snortlog" + curr_time + "/snortlog_line.txt"
-    #
     file = open(file_line, "w")
-    #
+    #Waiting for 3 seconds
     num_sec_to_sleep = 3
     file.close()
-    #
+    #changing chmod() the mode of path to the passed numeric mode such as Read, write, and execute by owner, group, and others.  
+    #sudo chmod 777 snortlog.txt 
     os.chmod(file_line, stat.S_IRWXU|stat.S_IRWXG|stat.S_IRWXO)   
-    #
+    #Copying the content of alert from snort output
     shutil.copy('/var/log/snort/alert', file_line)
-    #
+    #Stopping in time.sleep
     time.sleep(num_sec_to_sleep)
 
-#
+# chmod 777 text Function-----------------------------------------------------------------------------------------------------------
 def chmod_snort():
-    #
+    #finding the absolute path of the .log* files in the Desktop directory
     for reading_logsnort in glob.glob('/var/log/snort/tcpdump.log.*'):
-        #
+        #changing chmod() the mode of path to the passed numeric mode such as Read, write, and execute by owner, group, and others.  
+        #sudo chmod 777 snortlog.txt 
 	os.chmod(reading_logsnort, stat.S_IRWXU|stat.S_IRWXG|stat.S_IRWXO)
         print("Chmod tcpdump.log* is done")
 
-#	
+# tcpdump.log to new packet capture Function---------------------------------------------------------------------------------------- 	
 def snortlog_to_pcap(curr_time):
-    #
+    #finding the absolute path of the .log* files in the Desktop directory 
     for reading_output in glob.glob('/var/log/snort/tcpdump.log.*'):
-        #
+        #Creating a new packet capture from snort output
 	capture_file_pcap = "/tmp/snortlog" + curr_time + "/" "Snortlog" + curr_time + ".pcap"
-        #
+        #Waiting for 3 seconds
 	num_sec_to_sleep = 3
-        #
+        #Generating the tshark from tcpdump.log.xxxxxx to new packet capture
 	tcpdump = subprocess.Popen(["tshark", "-r", reading_output,
                                     "-w", capture_file_pcap],
                                     stdout=subprocess.PIPE)
-        #
+        #Stopping in time.sleep
 	time.sleep(num_sec_to_sleep)
-        #
+        #Exiting the program successfully
 	tcpdump.terminate()
 	
 ##Cleaning the alerts	
@@ -135,15 +138,15 @@ curr_time = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 os.system('systemctl stop snort')
 #Creating a directory with current time
 new_folder_log(curr_time)
-#Executing the 
+#Executing the tshark for the host with current time
 run_tshark_on_local_machine(curr_time)
-#
+#Exectung the snort from packet capture
 run_snort(curr_time)
-#
+# Copying snortlog to text
 snortlog_to_txt(curr_time)
-#
+# chmod 777 text
 chmod_snort()
-#
+# Generating tcpdump.log to new packet capture
 snortlog_to_pcap(curr_time)
-#
-print("Done")
+#End
+print("Successfully Done! :)")
